68f96506cebc8c407c2fc40c23db7dc4f76d047f
infreeJ <kasious97@naver.com>
2025-10-31 11:52:40 +0900

    [수정] 알림카드 UI 개선 및 파라미터 전달하여 재사용 가능한 컴포넌트로 설계 완료

--- staylog-frontend/src/domain/notification/components/NotificationCard.tsx
+++ staylog-frontend/src/domain/notification/components/NotificationCard.tsx
@@ -1,61 +1,45 @@
 import { Card, Image } from "react-bootstrap";
+import '../../../global/css/BootstrapCustomCss.css';
 
-function NotificationCard() {
+interface NotificationCardType {
+  imageUrl: string
+  date: string
+  title: string
+  typeName: string
+  message: string
+  timeAgo: string
+}
+
+/**
+ * 알림 카드 컴포넌트
+ * @author 이준혁
+ * @param imageUrl 이미지 주소
+ * @param date 체크인/체크아웃 또는 알림 생성일
+ * @param title 숙소명 or 작성자 등
+ * @param typeName 알림의 종류
+ * @param message 알림 내용
+ * @param timeAgo 알림 생성일로부터 얼마나 지났는지
+ * @returns NotificationCard (알림 카드)
+ */
+function NotificationCard({imageUrl, date, title, typeName, message, timeAgo}: NotificationCardType) {
   return (
     <>
-      <Card
-        style={{ width: '90%' }}
-        className="mx-auto position-relative bg-secondary-subtle border-0 rounded-3"
-      >
-        {/* 알림 점 (Red Dot) */}
-        <span
-          className="position-absolute"
-          style={{
-            top: '12px',
-            right: '12px',
-            width: '10px',
-            height: '10px',
-            backgroundColor: '#fd5353',
-            borderRadius: '50%',
-          }}
-        />
-
-        {/* Card.Body */}
+      <Card style={{ width: '90%', borderRadius: '10px 45px 10px 10px' }} className="mx-auto position-relative bg-secondary-subtle border-0 shadow-sm cursor-pointer mb-4">
+        <span className="position-absolute" style={{ top: '2px', right: '2px', width: '10px', height: '10px', backgroundColor: '#ee6f6fff', borderRadius: '50%', }} />
         <Card.Body className="d-flex align-items-center p-3">
-          {/* 1. 이미지 */}
-          <Image
-            src="https://picsum.photos/id/237/100/100" // 표시용 이미지
-            style={{
-              width: '85px',
-              height: '85px',
-              objectFit: 'cover',
-            }}
-            className="rounded-3"
-          />
-
+          <Image src={imageUrl} style={{ width: '85px', height: '85px', objectFit: 'cover', }} className="rounded-3" />
           <div className="flex-grow-1">
-            <Card.Text as="small" className="text-muted mb-1 d-block ms-3">
-              2025-08-31
-            </Card.Text>
-
+            <Card.Text as="small" className="text-muted mb-1 d-block ms-3 fs-8">{date}</Card.Text>
             <div className="d-flex justify-content-between align-items-baseline mb-1">
-              <Card.Title as="h6" className="fw-bold mb-0 ms-3">
-                브리드호텔 양양
-              </Card.Title>
-              <Card.Text as="small" className="mb-0 text-secondary">
-                Review
-              </Card.Text>
+              <Card.Title as="h6" className="fw-bold mb-0 ms-3 text-semiblack">{title}</Card.Title>
+              <Card.Text as="small" className="mb-0 text-secondary fs-9">{typeName}</Card.Text>
             </div>
 
             <hr className="my-2 border-secondary opacity-50" />
 
             <div className="d-flex justify-content-between align-items-baseline">
-              <Card.Text className="mb-0 fw-medium ms-3 fs-7">
-                신규 리뷰
-              </Card.Text>
-              <Card.Text as="small" className="mb-0 text-muted">
-                1분 전
-              </Card.Text>
+              <Card.Text className="mb-0 fw-medium ms-3 fs-8 text-semiblack">{message}</Card.Text>
+              <Card.Text as="small" className="mb-0 text-muted fs-10">{timeAgo}</Card.Text>
             </div>
           </div>
         </Card.Body>
