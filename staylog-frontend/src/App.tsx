fbc211e7aff592e5d93614901315140ad2d1e9fa
danjae1 <numbness98@naver.com>
2025-11-01 16:21:22 +0900

    [추가|수정] 리덕스 스토어에서 공통코드 관리하기

--- staylog-frontend/src/App.tsx
+++ staylog-frontend/src/App.tsx
@@ -3,11 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from "./global/components/Navbar"
 import useAuthJwt from "./domain/auth/hooks/useAuthJwt";
+import useCommonCodes from "./domain/common/hooks/useCommonCodes";
 
 function App() {
   const currentOutlet = useOutlet();
 
+  // JWT 토큰 검증 및 자동 로그아웃 타이머 설정
   useAuthJwt()
+
+  // 앱 실행 시 공통코드 조회 및 Redux에 저장
+  useCommonCodes()
 
   return (
     <>
